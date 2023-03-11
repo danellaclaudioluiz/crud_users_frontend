0091f43f848d368adfe667bbb2256b7f3199636e
danellaclaudioluiz <danellaclaudioluiz@gmail.com>
2023-03-11 20:58:10 -0300

    added layout css

--- src/module/Layout.tsx
+++ src/module/Layout.tsx
@@ -1,5 +1,13 @@
+import Style from './LayoutStyle.module.css'
+
 const Layout = () => {
-  return <div>This is layout</div>
+  return (
+    <article>
+      <header className={Style.header}>
+        <h1>Seja bem vindo ao sistema de usuários!</h1>
+      </header>
+    </article>
+  )
 }
 
 export default Layout
